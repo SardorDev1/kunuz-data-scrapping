580e936f0a4f8be0621e1a6d6ba9ccf9be4b9b28
SardorDev <sardordev1@mail.ru>
2023-08-03 20:59:03 +0500

    commit

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const app = express();
 const port = 3000;
 
 app.use(cors())
-const url = 'https://kun.uz';
+const url = 'https://kun.uz/news/category/tehnologia';
 
-app.get('/kunuz-small-news', (req, res) => {
+app.get('/', (req, res) => {
     axios.get(url)
         .then(response => {
             const html = response.data;
@@ -16,23 +16,25 @@ app.get('/kunuz-small-news', (req, res) => {
 
             const result = [];
 
-            $('div.top-news__small-items > div.row > div.col-md-6 > div.small-news').each((index, element) => {
-                const imgSrc = $(element).find('a.small-news__img > img').attr('src');
+            $('div.row > div.col-md-4 > div.news').each((index, element) => {
+                const imgSrc = $(element).find('a.news__img > img').attr('src');
                 const date = $(element).find('div.news-meta > span').text()
-                const title = $(element).find('div.small-news__content > a.small-news__title').text();
-                const description = $(element).find('div.small-news__content > div.small-news__description').text();
+                const title = $(element).find('a.news__title').text();
+                const link = $(element).find('a.news__title').attr('href');
+              
 
                 result.push({
                     imgSrc: imgSrc,
                     date: date,
                     title: title,
-                    description: description
+                    link: link
                 });
             });
 
             // Bu joyda API-ga yuborishni bajaramiz
             // result ma'lumotlarini JSON formatda qaytarish
             res.json(result);
+           
         })
         .catch(error => {
             console.log('Xato yuz berdi:', error);
